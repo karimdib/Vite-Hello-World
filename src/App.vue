78791aa8f037975754defa406e27907e322f16ec
karimdib <karim.dib2001@gmail.com>
2023-11-03 13:36:43 +0100

    hello

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script >
+export default{
+  data(){
+    return{
+      messaggio:'Hello World'
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div class="hello">{{ messaggio }}</div>
+  </div>
+</template>
+
+<style>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+.hello{
+  color: greenyellow;
+}
+</style>
